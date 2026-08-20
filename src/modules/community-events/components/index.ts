372b2629003cc07b8ca0b91bf5418bdb5b77f6e5
washingtonmsdj <washingtonmsdj@gmail.com>
2026-08-20 17:41:18 -0300

    refactor(events): move presentational components into community-events module

---
+++ src/modules/community-events/components/index.ts
@@ -0,0 +1,6 @@
+export { EventCard } from "./EventCard";
+export { EventHero } from "./EventHero";
+export { EventCTA } from "./EventCTA";
+export { EventTickets } from "./EventTickets";
+export { EventDescription } from "./EventDescription";
+export { EventTicketManager } from "./EventTicketManager";
