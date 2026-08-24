6de77fbc5685baa7f33bc9da5516c1c6be0b8782
washingtonmsdj <washingtonmsdj@gmail.com>
2026-08-24 18:38:34 -0300

    test(lgpd): include user export security suite in Heavy regression

---
+++ tests/regression-lgpd-user-export.test.ts
@@ -0,0 +1,3 @@
+import "./security/lgpd-edge-rollout-preflight-security.test.ts";
+import "./security/user-export-data-redaction-fixtures.test.ts";
+import "./security/user-export-data-v2-security.test.ts";
