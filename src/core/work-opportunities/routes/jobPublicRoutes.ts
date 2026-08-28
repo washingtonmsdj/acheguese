c0b2719b5b9bb1952be57a13ef6bb15129f893e3
washingtonmsdj <washingtonmsdj@gmail.com>
2026-08-28 08:34:50 -0300

    arch(config): use canonical module slugs in job routes

--- src/core/work-opportunities/routes/jobPublicRoutes.ts
+++ src/core/work-opportunities/routes/jobPublicRoutes.ts
@@ -1,4 +1,4 @@
-import { APP_MODULE_SLUGS, buildAppModulePath } from "@/config/moduleSlugs";
+import { APP_MODULE_SLUGS, buildAppModulePath } from "@/shared/config/moduleSlugs";
 import { TERRITORIAL_ROUTE_STATIC_SEGMENTS } from "@/core/routing/config/territorialRoutePatterns";
 
 export interface JobTerritoryRouteInput {
