eb3cc62be21dfcd7c4c0d31ae785991c35b11845
washingtonmsdj <washingtonmsdj@gmail.com>
2026-08-28 08:35:11 -0300

    arch(config): use canonical module slugs in tourist point routes

--- src/core/guide/tourist-points/routes/touristPointPublicRoutes.ts
+++ src/core/guide/tourist-points/routes/touristPointPublicRoutes.ts
@@ -1,4 +1,4 @@
-import { APP_MODULE_SLUGS, buildAppModulePath } from "@/config/moduleSlugs";
+import { APP_MODULE_SLUGS, buildAppModulePath } from "@/shared/config/moduleSlugs";
 import {
   TERRITORIAL_ROUTE_PARAMS,
   buildTerritorialModuleRoutePath,
