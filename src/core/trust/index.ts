4e6b7c6707b4b73b6882b28d7406de9f5ad1c155
washingtonmsdj <washingtonmsdj@gmail.com>
2026-08-29 16:34:10 -0300

    refactor(trust): bridge reusable UI to shared owner

--- src/core/trust/index.ts
+++ src/core/trust/index.ts
@@ -1,3 +1,8 @@
 export * from "./domain";
 export * from "./services";
-export * from "./components";
+export {
+  TrustFeedbackForm,
+  type TrustFeedbackReason,
+  type TrustFeedbackSubmission,
+  type TrustFeedbackTarget,
+} from "@/shared/components/trust/TrustFeedbackForm";
