9596975bea40189b61c5ac73ba134537639946fa
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-02 03:51:51 -0300

    chore(architecture): remove unused community launch facade

--- src/core/community-experience/config/communityLaunch.ts
+++
@@ -1,7 +0,0 @@
-export {
-  SALVADOR_COMMUNITY_LAUNCH_CLUSTER,
-  SALVADOR_COMMUNITY_LAUNCH_GROUP_SLUG,
-  isSalvadorCommunityLaunchTerritory,
-} from "@/core/community/config/communityLaunch";
-
-export type { CommunityLaunchTerritory } from "@/core/community/config/communityLaunch";
