51aa43358dac400ed1ba2ac904c08b262947189b
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-05 02:58:28 -0300

    refactor(g6): retire unused civic reports island

--- src/core/community/services/index.ts
+++ src/core/community/services/index.ts
@@ -1,4 +1,3 @@
 // Barrel export for community services
-export { CivicReportService } from "./CivicReportService";
 export { communityLocationService, CommunityLocationService } from "./CommunityLocationService";
 export { communityRolloutService, CommunityRolloutService } from "./CommunityRolloutService";
