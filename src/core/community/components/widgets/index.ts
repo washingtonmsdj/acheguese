04594c05ef55c90c9210ad13eb015b45ead55a47
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-05 06:35:18 -0300

    refactor(g6): remove orphaned community ui

--- src/core/community/components/widgets/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Barrel export para widgets da comunidade
- * Facilita imports e mantém organização
- */
-
-export { GroupsWidget } from "./GroupsWidget";
-export { TrendingWidget } from "./TrendingWidget";
-export { SponsoredWidget } from "./SponsoredWidget";
-export { WidgetSkeleton } from "./WidgetSkeleton";
