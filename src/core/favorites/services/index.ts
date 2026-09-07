60ed1e95a968e336be7c927dc46632709f1504aa
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-06 22:27:15 -0300

    refactor(favorites): narrow exports to active stores

--- src/core/favorites/services/index.ts
+++ src/core/favorites/services/index.ts
@@ -1,28 +1,16 @@
 export {
   getFavoriteStats,
-  getFavoritesByProfile,
-  getFavoritesWithProfiles,
-  getFavoritersOfProfile,
   getCurrentUserBusinessFavorites,
   isBusinessFavorited,
-  isFavorited,
 } from "./favorites.queries";
 
 export {
   addBusinessFavorite,
-  addFavorite,
   removeBusinessFavorite,
-  removeFavorite,
   setBusinessFavorite,
-  toggleFavorite,
 } from "./favorites.mutations";
 
-export type {
-  CreateFavoriteData,
-  FavoriteQuery,
-  FavoriteStats,
-  ProfileFavorite,
-} from "../types";
+export type { FavoriteStats } from "../types";
 
 export {
   BusinessFavoriteStore,
