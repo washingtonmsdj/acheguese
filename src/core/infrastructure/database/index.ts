6096ffafb892117c3b17aa41161e6871aca03337
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-09 10:41:44 -0300

    refactor(database): drop legacy ride repository export

--- src/core/infrastructure/database/index.ts
+++ src/core/infrastructure/database/index.ts
@@ -39,7 +39,6 @@ export { BaseRepository } from './repositories/BaseRepository';
 
 // Repositories Concretos
 export { ProfileRepository, type Profile } from './repositories/ProfileRepository';
-export { RideRepository, type Ride, type RideStatus } from './repositories/RideRepository';
 export { DriverRepository, type Driver } from './repositories/DriverRepository';
 export { BusinessRepository, type Business, type BusinessStatus, type BusinessCategory } from './repositories/BusinessRepository';
 export { ClassifiedRepository, type Classified, type ClassifiedStatus, type ClassifiedCondition, type ClassifiedReach } from './repositories/ClassifiedRepository';
