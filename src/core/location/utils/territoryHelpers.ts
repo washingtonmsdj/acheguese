90ad93afadfb97a95aaa9a235da78477a80f69e5
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-13 04:50:35 -0300

    fix(location): import territorial group owner

--- src/core/location/utils/territoryHelpers.ts
+++ src/core/location/utils/territoryHelpers.ts
@@ -5,7 +5,8 @@
  * de forma dinâmica, sem hardcoded.
  */
 
-import type { Location, TerritorialGroupWithMembers } from '../types';
+import type { Location } from '../types';
+import type { TerritorialGroupWithMembers } from '@/core/territorial';
 import type { ResolvedTerritory } from '@/core/routing/hooks/useResolveTerritoryFromUrl';
 
 /**
