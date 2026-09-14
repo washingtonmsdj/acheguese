ada997f822f85ecc18e1e9821ca144c012e6dd15
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-14 20:15:31 -0300

    Remove duplicate GastronomyUrlService bridge

--- src/modules/business/gastronomy/services/GastronomyUrlService.ts
+++
@@ -1,2 +0,0 @@
-export { GastronomyUrlService } from "@/core/verticals/gastronomy/services/GastronomyUrlService";
-export type { BusinessUrlContext } from "@/core/business/services/BusinessUrlService";
