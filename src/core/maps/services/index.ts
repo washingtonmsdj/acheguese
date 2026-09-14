fb3754292c008e4492b7f6cd0375f6b9d48dff66
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-14 14:29:07 -0300

    Remove geolocation ownership from maps services

--- src/core/maps/services/index.ts
+++ src/core/maps/services/index.ts
@@ -6,9 +6,4 @@
 
 export { MapEntityProjectionService, mapEntityProjection } from './MapEntityProjectionService';
 
-export { GeolocationService } from './GeolocationService';
-export type { GeolocationCoords, GeolocationResult, GeolocationOptions } from './GeolocationService';
-
 export { GeocodingService, geocodingService } from './MapGeocodingAdapter';
-
-
