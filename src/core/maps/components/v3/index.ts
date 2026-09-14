9fb25118f2c50caff2e171aacdb28e10a192cf7e
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-14 08:33:08 -0300

    perf(maps): expose lazy MapLibre owner

--- src/core/maps/components/v3/index.ts
+++ src/core/maps/components/v3/index.ts
@@ -9,5 +9,5 @@ export type { MapLayerToggleProps } from './MapLayerToggle';
 
 export { useViewportBridge } from './useViewportBridge';
 
-export { MapLibreAdapter } from './MapLibreAdapter';
-export type { MapLibreAdapterHandle, MapLibreAdapterProps } from './MapLibreAdapter';
+export { MapLibreAdapter } from './LazyMapLibreAdapter';
+export type { MapLibreAdapterHandle, MapLibreAdapterProps } from './LazyMapLibreAdapter';
