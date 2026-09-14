4759c39628d619f8118b53286493794108e4d809
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-14 18:32:41 -0300

    refactor(privacy): expose data protection contact route constant

--- src/shared/constants/legal.ts
+++ src/shared/constants/legal.ts
@@ -1,5 +1,6 @@
 export const TERMS_OF_SERVICE_PATH = "/termos";
 export const PRIVACY_POLICY_PATH = "/privacidade";
 export const SUPPORT_PATH = "/contato";
+export const DATA_PROTECTION_CONTACT_PATH = "/dpo";
 export const COMMUNITY_GUIDELINES_ANCHOR = "diretrizes-da-comunidade";
 export const COMMUNITY_GUIDELINES_PATH = `${TERMS_OF_SERVICE_PATH}#${COMMUNITY_GUIDELINES_ANCHOR}`;
