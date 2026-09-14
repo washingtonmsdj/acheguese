98146f0a6a255f93dda5a34c1d28d67765a00dbd
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-14 18:23:15 -0300

    refactor(account): add MFA setup route to account SSOT

--- src/core/routing/config/account.ts
+++ src/core/routing/config/account.ts
@@ -5,6 +5,7 @@ export const ACCOUNT_PATHS = {
   access: "/conta/seguranca#acesso",
   email: "/conta/seguranca#email",
   password: "/conta/seguranca#senha",
+  mfa: "/conta/seguranca#mfa",
   notifications: "/conta/notificacoes",
   privacy: "/conta/privacidade",
   exportData: "/conta/privacidade#exportar",
