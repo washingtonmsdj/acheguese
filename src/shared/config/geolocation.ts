b42b3fbcf1ef9e44dd6eaa9233135bd8fafea059
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-14 14:30:23 -0300

    Make geolocation policy the single timing owner

--- src/shared/config/geolocation.ts
+++ src/shared/config/geolocation.ts
@@ -1,11 +1,16 @@
+import { TIMEOUTS } from "@/shared/constants";
+
 export const GEOLOCATION_RUNTIME = {
   cacheKey: "robust_geolocation_cache_v1",
   cacheTtlMs: 15 * 60 * 1000,
-  requestTimeoutMs: 15_000,
+  requestTimeoutMs: TIMEOUTS.GPS_LOCATION,
   requestMaximumAgeMs: 10_000,
   watchMaximumAgeMs: 5_000,
+  mobileFastTimeoutMs: 8_000,
   mobileFastMaximumAgeMs: 60_000,
+  fallbackTimeoutExtensionMs: 5_000,
   desktopFallbackMaximumAgeMs: 30_000,
+  ipFallbackTimeoutMs: TIMEOUTS.IP_GEOLOCATION,
   retryDelayMs: 500,
   safetyTimeoutBufferMs: 2_000,
   backgroundRefreshDelayMs: 100,
