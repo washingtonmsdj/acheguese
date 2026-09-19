b6d39f1aba7e370c677107bef246c71aff9aef35
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-19 16:22:27 -0300

    cleanup(guide): use canonical tourist-point route owner in index.ts

--- src/modules/guide/index.ts
+++ src/modules/guide/index.ts
@@ -30,5 +30,4 @@ export { TouristPointService } from './services/TouristPointService';
 // Hooks
 export { useTouristPoints, useTouristPointsCount } from './hooks/useTouristPoints';
 export { useTouristPoint } from './hooks/useTouristPoint';
-export { useGuideUrls, TOURIST_POINTS_SLUG } from './hooks/useGuideUrls';
 export { useHasTouristPoints } from './hooks/useHasTouristPoints';
