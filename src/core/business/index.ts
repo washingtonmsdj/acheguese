ce075d1a6b2598a99a3671e88dd9372efc669954
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-21 09:23:14 -0300

    refactor(business): remove gastronomy from public barrel

--- src/core/business/index.ts
+++ src/core/business/index.ts
@@ -19,8 +19,6 @@ export type {
 export { DAY_NAMES } from './BusinessHoursService';
 export type * from './types';
 
-// Gastronomy queries
-export { hasGastronomyProfile } from './services/gastronomy.queries';
 
 // Utils
 export * from './utils';
