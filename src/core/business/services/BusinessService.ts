d77db72ca8bdecac7ad5396299fd80022ba6caeb
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-21 17:05:08 -0300

    refactor(identity): remove Business fuzzy slug facade

--- src/core/business/services/BusinessService.ts
+++ src/core/business/services/BusinessService.ts
@@ -23,7 +23,6 @@ export {
   isBusinessCommunityLinkEligibleByDataId,
   getBusinessBySlug,
   checkSlugExists,
-  getSlugsByPrefix,
   getBusinessesByIds,
   searchBusinessesByName,
   getProducts,
@@ -181,7 +180,6 @@ export class BusinessService {
     BusinessQueries.isBusinessCommunityLinkEligibleByDataId;
   static getBusinessBySlug = BusinessQueries.getBusinessBySlug;
   static checkSlugExists = BusinessQueries.checkSlugExists;
-  static getSlugsByPrefix = BusinessQueries.getSlugsByPrefix;
   static getBusinessesByIds = BusinessQueries.getBusinessesByIds;
   static searchBusinessesByName = BusinessQueries.searchBusinessesByName;
   static getProducts = BusinessQueries.getProducts;
