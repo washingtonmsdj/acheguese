a36d93a9fe1471c415793ed40fdc74099aa36655
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-21 14:36:14 -0300

    feat(messaging): export Business direct messaging facade

--- src/core/messaging/index.ts
+++ src/core/messaging/index.ts
@@ -1,4 +1,8 @@
 export {
+  BusinessDirectMessagingService,
+  businessDirectMessagingService,
+} from "./services/BusinessDirectMessagingService";
+export {
   ClassifiedMessagingService,
   classifiedMessagingService,
 } from "./services/ClassifiedMessagingService";
@@ -6,6 +10,15 @@ export {
   CommunityDirectMessagingService,
   communityDirectMessagingService,
 } from "./services/CommunityDirectMessagingService";
+export type {
+  BusinessDirectMessage,
+  BusinessDirectMessageCursor,
+  BusinessDirectReportReason,
+  BusinessDirectThreadCursor,
+  BusinessDirectThreadPreview,
+  CreateBusinessDirectThreadInput,
+  SendBusinessDirectMessageInput,
+} from "./businessDirectTypes";
 export type {
   ClassifiedConversation,
   ClassifiedConversationBlockReason,
