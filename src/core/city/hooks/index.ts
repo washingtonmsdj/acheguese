6f7c0d2fc3cf6e891f0d2d848f908c4df384f0bf
washingtonmsdj <washingtonmsdj@gmail.com>
2026-09-24 14:31:08 -0300

    cleanup(mvp): retire orphan city featured hook

--- src/core/city/hooks/index.ts
+++ src/core/city/hooks/index.ts
@@ -1,4 +1,3 @@
 export { useCityMetadata } from './useCityMetadata';
 export { useCityMetadataList } from './useCityMetadataList';
-export { useCityFeatured } from './useCityFeatured';
 export type { CityMetadata } from './useCityMetadata';
